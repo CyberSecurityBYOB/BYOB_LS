b5a6eb16bd9f8ebd1c91c6c1b2b0db11cd3a60fb
Alessandro Fazio <agil3b3ast@hotmail.it>
2016-07-31 21:11:51 +0200

    Added FileReader with simple configuration

---
+++ src/ConfigurationFileReader.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+FILE_PATH = 'testFile.txt'
+
+
+class ConfigurationFileReader:
+
+    def __init__(self):
+        self.configDict = {}
+        self.filePath = FILE_PATH
+
+    def readConfigurationFile(self):
+        with open(FILE_PATH, 'r') as f:
+            for line in f:
+                splitLine = line.split()
+                self.configDict[splitLine[0]] = splitLine[1:]
+
+if __name__ == "__main__":
+    fileReader = ConfigurationFileReader()
+    fileReader.readConfigurationFile()
+    for (key,value) in fileReader.configDict.iteritems() :
+        print key,value
