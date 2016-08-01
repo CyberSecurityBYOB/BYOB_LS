fa77c02ba6d0a6453d1999503ed8430db8fd654f
Alessandro Fazio <agil3b3ast@hotmail.it>
2016-08-01 18:15:25 +0200

    Added simple log with hard coded infinite loop

--- src/Bot.py
+++ src/Bot.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
 
 from ConfigurationFileReader import ConfigurationFileReader
-from urllib2 import Request, URLError, urlopen, HTTPError
+from urllib2 import Request, URLError, urlopen
+from time import sleep
+
+SEPARATOR = '##############################'
 
 # Read file
 fileReader = ConfigurationFileReader()
@@ -11,22 +14,40 @@ fileReader.readConfigurationFile()
 # Valid URL url = 'http://www.google.com'
 # Unreachable URL url = 'http://www.asjdsajdsakjdsaksadjkasd.com'
 # Invalid URL url = 'dsakjshdakjadshksdahjksad'
-url = 'http://www.google.com'
-#headers = {'User-Agent' : 'Mozilla 5.10'}
-#request = urllib2.Request(url, None, headers)
-request = Request(url)
-request.add_header('User-agent', 'Mozilla 5.10')
+url = 'https://www.google.com'
+# headers = {'User-Agent' : 'Mozilla 5.10'}
+# request = urllib2.Request(url, None, headers)
 
-# Getting the response
-try:
-    response = urlopen(request)
-    # Print the headers
-    print response.headers
-except URLError, e:
-    if hasattr(e,'code') :
-        print 'Error code : ' , e.code
-    if hasattr(e, 'reason') :
-        print 'Error reason  : ' , e.reason
-except ValueError, v:
-    print 'This URL is invalid'
+while True:
+
+    print 'Sleeping...'
+    sleep(1)
+    print 'Awake!'
+    for i in xrange(3) :
+        request = Request(url)
+        request.add_header('User-agent', 'Mozilla 5.10')
+
+        # Getting the response
+        try:
+            response = urlopen(request)
+            # Print the headers
+            print response.headers
+
+            with open('log.txt', 'a') as log:
+                log.write(SEPARATOR + '\n')
+                for key, val in response.headers.items() :
+                    log.write(''.join('{} : {}'.format(key, val)) + '\n')
+                log.write(SEPARATOR + '\n')
+
+        except URLError, e:
+            if hasattr(e,'code') :
+                print 'Error code : ' , e.code
+            if hasattr(e, 'reason') :
+                print 'Error reason  : ' , e.reason
+        except ValueError, v:
+            print 'This URL is invalid'
+
+    print 'Work Done! Sleeping...'
+    sleep(3)
+    print "Let's work!"
 
