e5146d5db09c7e26e7090d9053cd156931961ae0
Andrea Gennusa <andrea@andrebian>
2016-08-05 12:30:58 +0200

    added str to wrapper, prepared to read v3 files

--- src/RequestBuilder.py
+++ src/RequestBuilder.py
@@ -35,9 +35,13 @@ class RequestBuilder:
         worker.url = self._setSettingOrDefault(self.configurationFileDictionary[Constants.URLS][index], self.configurationFileDictionaryDefault[Constants.URLS][0])
         worker.frequency = frequency
         worker.contacts = int(self._setSettingOrDefault(self.configurationFileDictionary[Constants.CONTACTS][index], self.configurationFileDictionaryDefault[Constants.CONTACTS][0]))
-        worker.proxy = self.configurationFileDictionary[Constants.PROXY][0]
+        worker.proxy = self._setSettingOrDefault(self.configurationFileDictionary[Constants.PROXY][index], self.configurationFileDictionaryDefault[Constants.PROXY][0])
         worker.userAgent = self._setSettingOrDefault(self.configurationFileDictionary[Constants.USERAGENT][index], self.configurationFileDictionaryDefault[Constants.USERAGENT][0])
         worker.sleepModeDate = self._setSettingOrDefault(self.configurationFileDictionary[Constants.SLEEPMODEDATE][index], self.configurationFileDictionaryDefault[Constants.SLEEPMODEDATE][0])
+        worker.sleepModeMaxHour = int(self._setSettingOrDefault(self.configurationFileDictionary[Constants.SLEEPMODEMAXHOUR][index], self.configurationFileDictionaryDefault[Constants.SLEEPMODEMAXHOUR][0]))
+        worker.sleepModeMinHour = int(self._setSettingOrDefault(self.configurationFileDictionary[Constants.SLEEPMODEMINHOUR][index], self.configurationFileDictionaryDefault[Constants.SLEEPMODEMINHOUR][0]))
+
+        worker.repeats = self._setSettingOrDefault(self.configurationFileDictionary[Constants.REPEATS][index], self.configurationFileDictionaryDefault[Constants.REPEATS][0])
 
         self.workers.append(worker)
 
@@ -57,3 +61,14 @@ class WorkerWrapper :
         self.userAgent = ''
         self.frequency = 0
         self.sleepModeDate = ''
+        self.sleepModeMaxHour = 0
+        self.sleepModeMinHour = 0
+        self.repeats = 0
+
+    def __str__(self):
+        return 'Settings:\nUrl : ' + self.url +
+        '\nMax Contacts : ' + str(self.contacts) +
+        '\nProxy : ' + self.proxy +
+        '\nUserAgent : ' + self.userAgent +
+        '\nFrequency : ' + str(self.frequency) +
+        '\nSleep Mode : ' + self.sleepModeDate + ' from//to : ' + str(self.sleepModeMinHour) + '//' + str(self.sleepModeMaxHour) + ' repeats : ' + str(self.repeats)
